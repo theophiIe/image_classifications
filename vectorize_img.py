49a451a00995ae045feb7501c75c531de39d8b4d
theophiIe <theomolinatti@gmail.com>
2022-12-04 22:19:22 +0100

    [feature] : enregistre les vecteurs de chaque modèles dans des fichiers NPY

--- vectorize_img.py
+++ vectorize_img.py
@@ -1,46 +1,69 @@
-from dash import Dash, html
-import dash_cytoscape as cyto
 import glob
-import re
-import networkx as nx
+import os
+
 import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sns
+import re
 import tensorflow as tf
 import tensorflow_hub as hub
 
 from PIL import Image
-from scipy.spatial import distance
-
-model_google_1 = tf.keras.Sequential([hub.KerasLayer("https://tfhub.dev/google/imagenet/inception_v3/feature_vector/5", trainable=False, arguments=dict(batch_norm_momentum=0.997))])
 
 
 def extract(model, file, size):
-  file = Image.open(file).convert('L').resize(size)
-  # display(file)
+    file = Image.open(file).convert('L').resize(size)
+    file = np.stack((file,) * 3, axis=-1)
+    file = np.array(file) / 255.0
 
-  file = np.stack((file,)*3, axis=-1)
-  file = np.array(file) / 255.0
+    embedding = model.predict(file[np.newaxis, ...])
 
-  embedding = model.predict(file[np.newaxis, ...])
-  # print(embedding)
-  vgg16_feature_np = np.array(embedding)
-  flattended_feature = vgg16_feature_np.flatten()
+    vgg16_feature_np = np.array(embedding)
+    flattended_feature = vgg16_feature_np.flatten()
 
-  # print(len(flattended_feature))
-  # print(flattended_feature)
-  # print('-----------')
-  return flattended_feature
+    return flattended_feature
 
-def vectorized_images(model, size, images_path):
+
+def vectorized_images(model, size, paths):
     vectorized_img = []
-    names = list(map(lambda image_name: image_name[6:-4], sorted(glob.glob(images_path))))
-    for img in sorted(glob.glob(images_path)):
-        vectorized_img.append(extract(model, img, size))
-    return vectorized_img, names
+
+    for image in paths:
+        vectorized_img.append(extract(model, image, size))
+
+    return vectorized_img
+
 
 if __name__ == '__main__':
+    model_google_1 = tf.keras.Sequential(
+        [hub.KerasLayer("https://tfhub.dev/google/imagenet/inception_v3/feature_vector/5",
+                        trainable=False, arguments=dict(batch_norm_momentum=0.997))])
 
-    vectors, names = vectorized_images(model_google_1, (299,299), "./img/*")
-    np.save('vectors.npy', vectors)
+    model_google_2 = tf.keras.Sequential([hub.KerasLayer(
+        "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet1k_b0/feature_vector/2", trainable=False)])
+
+    model_google_3 = tf.keras.Sequential([hub.KerasLayer(
+        "https://tfhub.dev/google/imagenet/mobilenet_v3_large_075_224/feature_vector/5", trainable=False,
+        arguments=dict(batch_norm_momentum=0.997))])
+
+    model_tensorflow_1 = tf.keras.Sequential(
+        [hub.KerasLayer("https://tfhub.dev/tensorflow/efficientnet/lite0/feature-vector/2",
+                        input_shape=(224, 224) + (3,))])
+
+    images_paths = sorted(glob.glob("./img/*"))
+    images_names = list(map(lambda image_name: re.search(r'/([a-zA-Z0-9]+)\.', image_name).group()[1:-1], images_paths))
+
+    extract_images_google_1 = vectorized_images(model_google_1, (299, 299), images_paths)
+    extract_images_google_2 = vectorized_images(model_google_2, (224, 224), images_paths)
+    extract_images_google_3 = vectorized_images(model_google_3, (224, 224), images_paths)
+    extract_tensorflow_1 = vectorized_images(model_tensorflow_1, (224, 224), images_paths)
+
+    if not os.path.exists("./vectors"):
+        os.mkdir("./vectors")
+
+    else:
+        files = glob.glob('./vectors/*')
+        for f in files:
+            os.remove(f)
+
+    np.save('./vectors/vectors_google_1.npy', extract_images_google_1)
+    np.save('./vectors/vectors_google_2.npy', extract_images_google_2)
+    np.save('./vectors/vectors_google_3.npy', extract_images_google_3)
+    np.save('./vectors/vectors_tensorflow_4.npy', extract_tensorflow_1)
